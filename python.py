0271f90f604dfd7af6c1d5fd95e1617484d779fe
Neha <nehahr944@gmail.com>
2024-07-02 12:19:51 +0530

    done

---
+++ python.py
@@ -0,0 +1,17 @@
+var1=int(input("enter the number"))
+var2=float(input("enter the number"))
+print("sum of", var1, "and ",var2," is ",var1+var2)
+print("sum of", var1, "and" ,var2, "is ",var1-var2)
+print("sum of", var1, "and" ,var2, "is ",var1*var2)
+print("sum of",var1, "and" ,var2,"is",var1/var2)
+print("module of",var1, "and",var2,"is",var1%var2)
+
+print("the sum of",var1 ,"and")
+
+"""neha=1000
+neha+=500
+print(neha)"""
+
+
+var=2
+print(var"*")
